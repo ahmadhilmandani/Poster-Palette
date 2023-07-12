6befd5af1bc945e74bb96b17bf7c2717d23bf88b
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-07-12 20:56:45 +0700

    UPDATE navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,16 +1,21 @@
 import logo from "../assets/logo.svg"
+import { useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
+
 
 export default function Navbar() {
+  const url = useLocation()
+  console.log(url)
   return (
-    <nav className="w-full shadow-xl h-16 py-3 px-3 lg:px-20 flex justify-between items-center">
+    <nav className="w-full shadow-xl h-20 py-3 px-3 lg:px-20 flex justify-between items-center bg-brown-100">
       <img src={logo} alt="" className="w-[120px]" />
-      <div className="flex justify-between gap-20 text-sm">
-        <button>Home</button>
-        <button>Katalog poster kami</button>
-        <button>Pesan Desain Poster</button>
-        <button>Tentang Kami</button>
+      <div className="flex justify-between gap-20 text-base">
+        <Link to={'/'} className={`${url.pathname === '/' && 'font-semibold'}`}>Home</Link>
+        <Link to={'/'}>Katalog poster Kami</Link>
+        <Link to={'/'}>Pesan Desain Poster</Link>
+        <Link to={'/'}>Perjalanan Kami</Link>
       </div>
-      <button className="text-sm text-center px-10 py-2 bg-emerald-950 text-brown-300 font-medium rounded-md">Login</button>
+      <Link to={'/'} className="hover:border-b-0 hover:opacity-90 text-sm text-center px-10 py-2 bg-emerald-950 text-brown-300 font-medium rounded-md">Login</Link>
     </nav>
   )
 }
