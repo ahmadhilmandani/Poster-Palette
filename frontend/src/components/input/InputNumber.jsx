e653675885cb3eb9ccfcba2594912ba647ab8fcb
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-07-20 14:46:21 +0700

    UPDATE inputNumber components to be rounded

--- frontend/src/components/input/InputNumber.jsx
+++ frontend/src/components/input/InputNumber.jsx
@@ -6,13 +6,13 @@ export default function InputNumber({ leftItem, quantity, setQuantity }) {
   return (
     <>
       <div className="flex items-center">
-        <button className="w-10 border border-slate-300 py-1" onClick={() => { setQuantity(quantity - 1) }}>-</button>
+        <button className="w-10 border border-slate-300 py-1 rounded-s-[4px]" onClick={() => { setQuantity(quantity - 1) }}>-</button>
         <input
           type="number"
           className="custom-input outline-none py-1 w-20 text-center border border-slate-300 bg-transparent"
           value={quantity}
         />
-        <button className="w-10 border border-slate-300 py-1" onClick={() => { setQuantity(quantity + 1) }}>+</button>
+        <button className="w-10 border border-slate-300 py-1 rounded-e-[4px]" onClick={() => { setQuantity(quantity + 1) }}>+</button>
         <div className="text-slate-300 ml-5">tersisa {leftItem}</div>
       </div>
     </>
