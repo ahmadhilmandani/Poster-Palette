1c5b8edb72f948cca3f72da5ab272b86c572a9f1
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-07-20 14:45:24 +0700

    UPDATE change components name of InputRadioColor to InputRadio

--- frontend/src/components/input/InputRadio.jsx
+++ frontend/src/components/input/InputRadio.jsx
@@ -3,27 +3,27 @@
 import { IconCheck } from "@tabler/icons-react"
 
 
-export default function InputRadioColor({ inputId, label, InputName, inputVal, setColorChoosed, colorChoosed }) {
+export default function InputRadio({ inputId, children, InputName, inputVal, choosed, setChoosed }) {
   const styleData = {
     clipPath: "polygon(100% 0, 0% 100%, 100% 100%)"
   }
 
   return (
     <>
-      <label htmlFor={inputId} className={`block border px-6 py-1 relative ${colorChoosed === inputVal ? 'border-emerald-950' : 'border-slate-300'}`}>
-        {label}
+      <label htmlFor={inputId} className={`flex flex-col rounded-[4px] items-center border px-6 py-2 relative ${choosed === inputVal ? 'border-emerald-950' : 'border-slate-300'}`}>
+        {children}
 
         {
-          colorChoosed === inputVal &&
+          choosed === inputVal &&
           <span style={styleData} className="bg-emerald-950 h-5 aspect-[32/28] absolute bottom-0 right-0">
             <IconCheck size={9} color="white" stroke={4} className="absolute bottom-[1.2px] right-[1.2px]" />
           </span>
         }
-        
+
       </label>
       <input type="radio" id={inputId} name={InputName} value={inputVal} className="hidden"
         onClick={(e) => {
-          setColorChoosed(e.target.value)
+          setChoosed(e.target.value)
         }} />
     </>
   )
