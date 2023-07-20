0665aa3601aa3c40ceba51399d0eb9c189c8cdfd
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-07-20 14:46:05 +0700

    ADD pageBanner components

---
+++ frontend/src/components/pageBanner/pageBanner.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import roundedRectangle from "../../assets/rounded-rectangle.png"
+
+
+export default function PageBanner({ h1, children }) {
+  return (
+    <div className="bg-brown-100 w-full h-[316px] mt-20 flex flex-col items-center justify-center relative">
+      <h1>{h1}</h1>
+      <p className="text-brown-400 mt-4 max-w-[800px] text-center">{children}
+      </p>
+      <img src={roundedRectangle} alt="" className="absolute top-0 left-0 h-3/4" />
+      <img src={roundedRectangle} alt="" className="absolute right-0 bottom-0 rotate-180 h-3/4" />
+    </div>
+  )
+}
