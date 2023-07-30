6099e072326e771e1f433a566a436246d0151dbf
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-07-31 06:50:48 +0700

    UPDATE sign up consume API to register user

--- frontend/src/components/input/InputString.jsx
+++ frontend/src/components/input/InputString.jsx
@@ -2,11 +2,11 @@ import { IconEye, IconEyeOff } from "@tabler/icons-react";
 import { useState } from "react";
 
 /* eslint-disable react/prop-types */
-export default function InputString({ name, id, value, placeholder, type, setIsPasswordVisible = null, isPasswordVisible = null }) {
+export default function InputString({ name, id, setValue, placeholder, type, setIsPasswordVisible = null, isPasswordVisible = null }) {
   const [isActive, setIsActive] = useState(false)
   return (
     <div className={`flex items-center justify-center gap-4 w-fit ${isActive === true ? 'border-b border-brown-400' : 'border-b border-b-slate-200'} max-w-[280px] w-full`}>
-      <input type={`${type === 'password' ? (isPasswordVisible === true ? ('text') : ('password')) : (type)}`} name={name} id={id} value={value} placeholder={placeholder} className="block px-3 py-2 w-full border-0 outline-none bg-transparent mt-4" onFocus={() => { setIsActive(true) }} onBlur={() => { setIsActive(false) }} />
+      <input type={`${type === 'password' ? (isPasswordVisible === true ? ('text') : ('password')) : (type)}`} name={name} id={id} placeholder={placeholder} className="block px-3 py-2 w-full border-0 outline-none bg-transparent mt-4" onChange={(e) => { setValue(e.target.value) }} onFocus={() => { setIsActive(true) }} onBlur={() => { setIsActive(false) }} />
       {type === 'password' ?
         isPasswordVisible === true ?
           <IconEye color="rgb(164,133,103)" onClick={() => { setIsPasswordVisible(!isPasswordVisible) }} />
--- frontend/src/pages/SignUp/signUp.jsx
+++ frontend/src/pages/SignUp/signUp.jsx
@@ -3,25 +3,57 @@ import InputString from "../../components/input/InputString";
 import { Link } from "react-router-dom";
 
 import headerImg from "../../assets/hero-img.png"
-
+import axios from 'axios';
 
 export default function SignUp() {
+  const [loading, setLoading] = useState(false)
+  const [username, setUsername] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [address, setAddress] = useState(null)
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+  const handleSignUp = () => {
+    setLoading(true)
+    const requestData = new URLSearchParams();
+    requestData.append('username', username);
+    requestData.append('email', email);
+    requestData.append('password', password);
+    requestData.append('address', address);
+
+    console.log(requestData)
+
+    axios.post('http://localhost:3000/auth/sign-up', requestData.toString(), {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    }).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    }).finally(() => {
+      setLoading(false)
+    })
+  }
 
 
   return (
     <>
+      {loading === true &&
+        <div className="bg-white w-full min-h-screen fixed top-0 bottom-0 left-0 right-0 z-[400]">
+        </div>
+      }
       <div className="w-full h-screen flex justify-center items-center bg-brown-50 mt-20">
         <div className="rounded-md bg-white max-w-[800px] w-full min-h-[560px] flex justify-center shadow-xl p-10 gap-x-32">
           <div className="w-1/2">
             <div className="border-b border-brown-400 px-5 py-3 font-bold text-center w-full">Sign Up</div>
             <div className="mt-4">
-              <InputString type={'text'} placeholder={'Masukkan Nama Anda'} />
-              <InputString type={'email'} placeholder={'Masukkan Email Anda'} />
-              <InputString type={'password'} placeholder={'Masukkan Password Anda'} setIsPasswordVisible={setIsPasswordVisible} isPasswordVisible={isPasswordVisible} />
-              <InputString type={'text'} placeholder={'Masukkan Alamat Lengkap Anda'} />
+              <InputString type={'text'} placeholder={'Masukkan Nama Anda'} setValue={setUsername} />
+              <InputString type={'email'} placeholder={'Masukkan Email Anda'} setValue={setEmail} />
+              <InputString type={'password'} placeholder={'Masukkan Password Anda'} setValue={setPassword} setIsPasswordVisible={setIsPasswordVisible} isPasswordVisible={isPasswordVisible} />
+              <InputString type={'text'} placeholder={'Masukkan Alamat Lengkap Anda'} setValue={setAddress} />
               <span className="text-sm text-slate-500 mt-2 px-3">NB: digunakan untuk mengirimkan poster anda</span>
-              <button className="block w-full py-4 rounded-lg mt-6 bg-emerald-950 text-brown-300">
+              <button className="block w-full py-4 rounded-lg mt-6 bg-emerald-950 text-brown-300" onClick={() => { handleSignUp() }}>
                 Sign Up
               </button>
               <p className="mt-10">Sudah punya akun? <Link to={'/login'} className="text-brown-400">Login aja!</Link></p>
